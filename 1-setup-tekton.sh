5795c9bd83d5f39823d08dec23b4725986789450
cheng-yen <cheng-yen@polytechnique.edu>
2023-06-19 16:48:52 +0200

    waiting for ready

--- 1-setup-tekton.sh
+++ 1-setup-tekton.sh
@@ -10,10 +10,18 @@ green=$(tput setaf 2) || true
 echo "${bb}Loading the latest tekton pipeline... ${nn}"
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 
+echo ""
+
+echo "${bb}Waiting for tekton pipeline being ready... ${nn}"
+kubectl wait --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml --for=condition=Ready
+
 
 # Chaine 
 echo "${bb}Loading the tekton chain v0.14.0... ${nn}"
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/chains/previous/v0.14.0/release.yaml
+
+echo "${bb}Waiting for tekton chain being ready... ${nn}"
+kubectl wait --filename https://storage.googleapis.com/tekton-releases/chains/previous/v0.14.0/release.yaml --for=condition=Ready
 
 echo "${green}Loading tekton completed.${nn}"
 
